a1af4bdecb041b8803bf28edb803c4b0df61e97d
Berta2109 <129122453+Berta2109@users.noreply.github.com>
2023-07-20 12:09:00 +0200

    Update SerialHystrixConfiguration.java

--- SerialHystrixConfiguration.java
+++ SerialHystrixConfiguration.java
@@ -1,7 +1,7 @@
 /**
  * Copyright 2016 Netflix, Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");23
+ * Licensed under the Apache License, Version 2.0 (the "License");23a
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at 1
  *
